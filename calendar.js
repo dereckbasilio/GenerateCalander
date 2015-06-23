d9ec2194a3a2358fbdec34aace2101624e328031
Dereck Basilio <dereck@meshify.com>
2015-06-23 12:13:32 -0500

    Add month, year, and week constants

--- calendar.js
+++ calendar.js
@@ -1,5 +1,24 @@
 $(function() {
 	console.log("File Found");
+
+	var currentYear = 2015;
+	var currentMonth = 5;
+	var daysInWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+	var daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];	
+	var months = {
+		"january": [31, -1],
+		"febuary": [28, -1],
+		"march": [31, -1],
+		"april": [30, -1]
+		"may": [31, -1],
+		"june": [30, -1],
+		"july": [31, -1],
+		"august": [31, -1],
+		"september": [30, -1],
+		"october": [31, -1],
+		"november": [30, -1],
+		"december": [31, -1]
+	}
 	var calBody = $("#calBody");
 	var generateCalendar = function(genTable){
 		dayStart = 0;
